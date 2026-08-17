6a28ab165cf666e792ca850c3368bfbb256ebb28
briangacheru <briangacheru14@gmail.com>
2026-08-17 20:07:00 +0300

    Show discussion message bubble next to task # on writer task list pages
    Mirrors the sudo/ admin change: batch-loads tbl_task_comments counts
    per page and renders a comment-dots icon next to the task ID when a
    task has messages, linking to the writer view-task's discussion
    section (#discussionCardBody, which differs from the admin page's
    #discussionBody id).

--- unconfirmed.php
+++ unconfirmed.php
@@ -69,6 +69,13 @@
                                         </thead>
                                         <tbody class="list" id="table-simple-pagination-body">
                                         <?php
+                                            $taskCommentCounts = [];
+                                            $commentCountsResult = mysqli_query($con, "SELECT task_id, COUNT(*) AS cnt FROM tbl_task_comments GROUP BY task_id");
+                                            if ($commentCountsResult) {
+                                                while ($ccRow = mysqli_fetch_assoc($commentCountsResult)) {
+                                                    $taskCommentCounts[$ccRow['task_id']] = (int) $ccRow['cnt'];
+                                                }
+                                            }
                                             $query=mysqli_query($con,"select * from tbltasks WHERE is_deleted = 0 AND is_confirmed = 1 AND email = '$aid' ORDER BY id DESC");
                                             $cnt=1;
                                             while($row=mysqli_fetch_array($query))
@@ -142,7 +149,12 @@
                                                     <input class="form-check-input" type="checkbox" id="simple-pagination-item-<?php echo $cnt; ?>" data-bulk-select-row="data-bulk-select-row" value="<?php echo $row['id']; ?>" name="taskIds[]"/>
                                                 </div>
                                             </td>
-                                            <td class="align-middle white-space-nowrap fw-semi-bold text-900"><span class='ms-3'><?php echo $row['id']; ?></span></td>
+                                            <td class="align-middle white-space-nowrap fw-semi-bold text-900">
+                                                <span class='ms-3'><?php echo $row['id']; ?></span>
+                                                <?php if (!empty($taskCommentCounts[$row['id']])): ?>
+                                                    <a href="view-task?task_id=<?php echo $encodedId; ?>#discussionCardBody" class="ms-1 text-info" data-bs-toggle="tooltip" data-bs-placement="top" title="<?php echo $taskCommentCounts[$row['id']]; ?> message<?php echo $taskCommentCounts[$row['id']] > 1 ? 's' : ''; ?> in discussion"><i class="fas fa-comment-dots"></i></a>
+                                                <?php endif; ?>
+                                            </td>
                                             <td>
                                                 <div class="d-flex align-items-center position-relative">
                                                     <div class="flex-1">
